4719df1f3961e37d2cf7928a20e4d383bfb169e2
hossam26644 <hossam26644@live.com>
2025-12-03 15:22:16 +0000

    Refactor simulate_sfs.py to handle branch SFS input and improve mutation simulation logging

--- snakemake/scripts/simulate_sfs.py
+++ snakemake/scripts/simulate_sfs.py
@@ -1,6 +1,7 @@
 import msprime
 import demes
 import sys
+from distutils.util import strtobool
 
 # Inputs
 seed = int(sys.argv[1])
@@ -9,10 +10,8 @@ sequence_length = int(sys.argv[3])
 mutation_rate = float(sys.argv[4])
 recombination_rate = float(sys.argv[5])
 output_path = sys.argv[6]
-branch_sfs = sys.argv[7]
+branch_sfs = strtobool(sys.argv[7])
 
-branch_sfs = False
-# Load demography
 graph = demes.load(graph_path)
 demography = msprime.Demography.from_demes(graph)
 
@@ -24,10 +23,12 @@ ts = msprime.sim_ancestry(
     sequence_length=sequence_length,
     recombination_rate=recombination_rate,
     demography=demography,
-    model=msprime.SmcKApproxCoalescent()
+    model=msprime.SmcKApproxCoalescent(hull_offset=1)
 )
 if not branch_sfs:
+    print("Adding mutations to tree sequence")
     ts = msprime.sim_mutations(ts, rate=mutation_rate, random_seed=seed)
+
 mode = "branch" if branch_sfs else "site"
 # Get SFS
 sfs = ts.allele_frequency_spectrum(polarised=True, span_normalise=branch_sfs, mode=mode)[1:-1]
