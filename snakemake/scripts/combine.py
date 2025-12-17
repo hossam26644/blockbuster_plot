01f0329b4983daeb62c0f92253b516bb32efeda6
hossam26644 <hossam26644@live.com>
2025-12-17 20:01:25 +0000

    support different time units (Ne generations)

--- snakemake/scripts/combine.py
+++ snakemake/scripts/combine.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 import sys
-
+import pandas as pd
 
 # Root folder where subdirectories contain demes.yml
 root_dir = sys.argv[1]
@@ -11,6 +11,25 @@ output_file = sys.argv[3]
 combined_demes = []
 time_units = None
 
+def ne_to_generations(data):
+    assert len(data["demes"]) == 1, "Expected exactly one deme for Ne to generations conversion."
+
+    deme = data["demes"][0]
+    epochs = deme["epochs"]
+    
+    epochs = pd.DataFrame(epochs)
+    epochs = epochs.sort_values(by="end_time", ascending=False).reset_index(drop=True)
+    epochs["size_after"] = epochs["start_size"].shift(-1)
+    epochs.at[len(epochs)-1, "size_after"] = 1
+    epochs["end_time"] = epochs["size_after"] * epochs["end_time"]
+    #epochs["end_time"] = epochs["start_size"] * epochs["end_time"]
+
+    deme["epochs"] = epochs[["start_size", "end_time"]].to_dict(orient="records")
+
+    data["demes"] = [deme]
+    return data
+
+time_units = None
 # Walk through all subdirectories
 for dirpath, dirnames, filenames in os.walk(root_dir):
     if f"{blocks}_epochs.yml" in filenames:
@@ -18,10 +37,12 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
         with open(file_path) as f:
             data = yaml.safe_load(f)
 
-        # Set time_units (assume all are the same)
-        if time_units is None:
-            time_units = data.get("time_units", "generations")
+        assert time_units is None or time_units == data["time_units"], "Inconsistent time units across files."
+        time_units = data["time_units"]
 
+        if time_units == "Ne generations":
+            data["time_units"] = "generations"
+            data = ne_to_generations(data)
         if "demes" in data and len(data["demes"]) == 1:
             deme = data["demes"][0]
             
@@ -33,7 +54,7 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
 
 # Combine into final structure
 combined_model = {
-    "time_units": time_units,
+    "time_units": "generations",
     "demes": combined_demes
 }
 
